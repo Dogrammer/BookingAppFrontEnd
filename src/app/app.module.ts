c6802fafa37dfffc953b0e08d031113dcc810b59
Nevio Marjanovic <nevio.marjanovic@fleksbit.hr>
2020-07-13 20:00:59 +0200

    create home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { SiteLayoutComponent } from './layout/site-layout/site-layout.component'
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { HomeComponent } from './home/home.component';
 import { SiteNavbarComponent } from './layout/site-layout/site-navbar/site-navbar.component';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { SiteNavbarComponent } from './layout/site-layout/site-navbar/site-navba
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgImageSliderModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  
+  images = ['../../assets/homepage/pic1.jpg', '../../assets/homepage/pic2.jpg', '../../assets/homepage/pic3.jpg' ]
   constructor() { }
 
   ngOnInit(): void {
